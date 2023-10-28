c337b65f17ce93dea88faf3f640e87bc60b17bf8
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-28 04:30:26 +0200

    11. Zombie (A.C)

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function that makes 5 zombie process
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 5; i++)
+	{
+		int pid = fork();
+
+		if (pid == 0)
+		{
+			/*child proc*/
+			printf("Zombie process created, PID: %i\n", getpid());
+
+			exit(0);
+		}
+		else if (pid > 0)
+		{
+			/*parent proc*/
+
+			sleep(1);
+		}
+		else
+		{
+			fprintf(stderr, "failed");
+		}
+	}
+}
