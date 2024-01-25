867ca7d6c4e8417893b9cf2df6d09cd49d9418d6
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-01-25 11:01:57 +0200

    1. Install a package (automated command)

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,13 @@
 #!/usr/bin/pup
 # Install flask 2.1.0
 
-package { 'python3-pip':
-  ensure => installed,
-}
 
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
-package {'werkzeug':
+package {'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
 }
