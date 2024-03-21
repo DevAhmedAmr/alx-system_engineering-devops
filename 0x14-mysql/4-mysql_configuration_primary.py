95dc7de39f606cbfd90c5e70ee70e89c81f6f7b3
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-03-21 12:42:35 +0200

    added tutorial link (automated command)

--- 0x14-mysql/4-mysql_configuration_primary.py
+++ 0x14-mysql/4-mysql_configuration_primary.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 # script gets MySQL installed on both my web-01 and web-02 servers.
-from fabric.api import * 
+from fabric.api import *
 from check_and_update_config_line import check_and_update_config_line
 
-env.hosts=["34.227.93.198","54.166.169.232","34.232.65.82"]
-env.key_filename=r"C:\Users\ahmed\.ssh\pk3.pem"
-env.user="ubuntu"
-file_path="/etc/mysql/mysql.conf.d/mysqld.cnf"
-database_name="tyrell_corp"
-curr_path=r"S:\alx\alx-system_engineering-devops\0x14-mysql\sql"
+# followed this tutorial
+# ? https://phoenixnap.com/kb/mysql-master-slave-replication
+env.hosts = ["34.227.93.198", "54.166.169.232", "34.232.65.82"]
+env.key_filename = r"C:\Users\ahmed\.ssh\pk3.pem"
+env.user = "ubuntu"
+file_path = "/etc/mysql/mysql.conf.d/mysqld.cnf"
+database_name = "tyrell_corp"
+curr_path = r"S:\alx\alx-system_engineering-devops\0x14-mysql\sql"
 
 # sudo("ufw allow from 54.166.169.232 to any port 3306")
 
-check_and_update_config_line("bind-address","# bind-address = 127.0.0.1",file_path)
-check_and_update_config_line("server-id","server-id = 1",file_path)
-check_and_update_config_line("log_bin","log_bin = /var/log/mysql/mysql-bin.log",file_path)
-check_and_update_config_line("binlog_do_db",f"binlog_do_db = {database_name}",file_path)
+check_and_update_config_line("bind-address", "# bind-address = 127.0.0.1", file_path)
+check_and_update_config_line("server-id", "server-id = 1", file_path)
+check_and_update_config_line(
+    "log_bin", "log_bin = /var/log/mysql/mysql-bin.log", file_path
+)
+check_and_update_config_line(
+    "binlog_do_db", f"binlog_do_db = {database_name}", file_path
+)
 
 # Restart MySQL service
 sudo("sudo systemctl restart mysql")
@@ -33,13 +39,7 @@ sudo(f"mysqldump -u root -p {database_name} > {database_name}.sql")
 sudo("echo 'UNLOCK TABLES;' | mysql -u root -p")
 
 # Get the SQL dump file from the remote server to the local machine
-get(f"./{database_name}.sql",curr_path)
-
-
-
-
-
-
+get(f"./{database_name}.sql", curr_path)
 
 
 # Search for the line containing the word "bind-address" and comment it out
@@ -55,4 +55,3 @@ get(f"./{database_name}.sql",curr_path)
 # sudo(f"""sed -i 's/include_database_name/{database_name}/' {file_path}""")
 
 # print( "' #binlog_do_db = include_database_name' Line uncommented and replaced.")
-
