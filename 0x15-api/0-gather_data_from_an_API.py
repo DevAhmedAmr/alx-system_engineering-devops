b9b6dac23f5a455078a6d273ce18e4f8b35aa9b4
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-03-30 14:28:10 +0200

    task 0 - Gather data from an API (automated command)

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import requests
+import sys
+import json
+id = sys.argv[1]
+
+r = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
+EMPLOYEE_NAME =json.loads( r.text)["name"]
+
+r = requests.get(f'https://jsonplaceholder.typicode.com/todos/?userId={id}')
+
+NUMBER_OF_DONE_TASKS=0
+todo=json.loads( r.text)
+TOTAL_NUMBER_OF_TASKS=len(todo)
+for task in todo:
+    
+    if task["userId"]==int(id):
+        
+        if task["completed"]== True:
+            NUMBER_OF_DONE_TASKS+=1
+            
+print(f"Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+
+for task in todo:
+    if task["completed"] == True:
+        print(" ","\t",task["title"])
