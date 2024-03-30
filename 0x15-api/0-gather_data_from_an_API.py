666cbcdc8bfe6eb3849e352a771a33b9c451673e
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-03-30 15:12:37 +0200

    task 0 - Gather data from an API (automated command)

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,31 +4,37 @@
 import requests
 import sys
 import json
+
 """place holder"""
+
+
 def Gather_data():
-	"""_summary_
-	"""
-	id = sys.argv[1]
+    """_summary_"""
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com"
 
-	r = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
-	EMPLOYEE_NAME =json.loads( r.text)["name"]
+    r = requests.get(f"{url}/users/{id}")
+    EMPLOYEE_NAME = json.loads(r.text)["name"]
 
-	r = requests.get(f'https://jsonplaceholder.typicode.com/todos/?userId={id}')
+    r = requests.get(f"{url}/todos/?userId={id}")
 
-	NUMBER_OF_DONE_TASKS=0
-	todo=json.loads( r.text)
-	TOTAL_NUMBER_OF_TASKS=len(todo)
-	for task in todo:
-		
-		if task["userId"]==int(id):
-			
-			if task["completed"]== True:
-				NUMBER_OF_DONE_TASKS+=1
-				
-	print(f"Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    NUMBER_OF_DONE_TASKS = 0
+    todo = json.loads(r.text)
+    TOTAL_NUMBER_OF_TASKS = len(todo)
+    for task in todo:
 
-	for task in todo:
-		if task["completed"] == True:
-			print(" ","\t",task["title"])
+        if task["userId"] == int(id):
+
+            if task["completed"] is True:
+                NUMBER_OF_DONE_TASKS += 1
+
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks", end="")
+    print(f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+
+    for task in todo:
+        if task["completed"] is True:
+            print(" ", "\t", task["title"])
+
+
 if __name__ == "__main__":
     Gather_data()
