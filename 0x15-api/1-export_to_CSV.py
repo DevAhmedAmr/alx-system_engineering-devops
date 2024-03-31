65296ca9c14776d26499a542f15431200990319b
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-03-31 07:37:23 +0200

    sorted modules alphabeticaly (automated command)

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python3
+"""place holder"""
+import csv
+import json
+import requests
+import sys
+
+"""place holder"""
+fileName="USER_ID.csv"
+
+def Gather_data():
+    """get data from api"""
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com"
+
+    r = requests.get(f"{url}/users/{id}")
+    EMPLOYEE_NAME = json.loads(r.text)["name"]
+
+    r = requests.get(f"{url}/todos/?userId={id}")
+    todo = json.loads(r.text)
+
+
+    create_file_if_Not_Exist(fileName)
+        
+    for task in todo:
+        data = f""" "{task["userId"]}","{EMPLOYEE_NAME}","{task["completed"]}","{task["title"]}" """.strip()
+        save_csv(fileName,data)
+        
+            
+    
+def create_file_if_Not_Exist(file_Name):
+    try:
+        open(f"./{file_Name}", 'x')
+    except FileExistsError:
+        pass        
+    
+def save_csv(file_name,data):
+        with open(f"./{file_name}","a") as file:
+            file.write(data+"\n")
+    
+if __name__ == "__main__":
+    Gather_data()
