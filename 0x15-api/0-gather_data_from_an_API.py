aa9be906d1f570189da7981591e763c7cae8a630
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-03-31 05:46:09 +0200

    sorted modules alphabeticaly (automated command)

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,14 +13,14 @@ def Gather_data():
 
     todos = requests.get(
         "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true"
-        .format(argv[1]))
+        .format(sys.argv[1]))
     name = requests.get(
         "https://jsonplaceholder.typicode.com/users?id={}"
-        .format(argv[1]))
+        .format(sys.argv[1]))
     name = name.json()
     name = name[0]["name"]
     todo = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(sys.argv[1]))
     todo = todo.json()
     todo = len(todo)
     todos = todos.json()
