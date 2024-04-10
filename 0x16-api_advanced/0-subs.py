0f7a61d4ec8d5194e86a0ccc6a2110e55fc5f786
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-04-10 21:47:44 +0200

    (automated command)

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,16 +6,16 @@ import sys
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
-
-    sub_info = requests.get(
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, return 0.
+    """
+    req = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "My-User-Agent"},
-        allow_redirects=False,
+        headers={"User-Agent": "Custom"},
     )
-    if sub_info.status_code >= 300:
-        return 0
 
-    return sub_info.json().get("data").get("subscribers")
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
+        return 0
