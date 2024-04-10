96e1bc64a29c86191b2dfc015c8ebe37c240c195
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-04-10 21:23:47 +0200

    task0 (automated command)

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 """dependencies"""
-import requests
 import json
+import requests
 import sys
 
 myUser_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
@@ -24,5 +24,8 @@ def number_of_subscribers(subreddit):
 
 
 if __name__ == "__main__":
-    x = number_of_subscribers(sys.argv[1])
-    print(x)
+    number_of_subscribers = __import__("0-subs").number_of_subscribers
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
