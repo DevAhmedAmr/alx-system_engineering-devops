ad82c14d30ed42223acc5b565d20657e5f3350a0
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-04-10 21:19:50 +0200

    task0 (automated command)

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,11 +3,13 @@ import requests
 import json
 import sys
 
+"""dependencies"""
 myUser_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
 user_agent = {"User-Agent": myUser_agent}
 
 
 def number_of_subscribers(subreddit):
+    """function to return number of subscriber of a subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     r = requests.get(
         url,
