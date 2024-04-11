6f52f7bfa098981a92a47d868439f3076ff7cb10
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-04-11 02:01:22 +0200

    (automated command)

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""dependencies"""
+import requests, json
+
+
+def top_ten(subreddit):
+    """a function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit."""
+
+    r = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
+    Data: dict = r.json()
+    if r.status_code == 200:
+        for i in range(len(Data["data"]["children"])):
+            print(Data["data"]["children"][i]["data"]["title"])
+    else:
+        print(None)
+
+
+if "__main__" == __name__:
+    top_ten("programming")
