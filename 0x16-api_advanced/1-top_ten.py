f2090086b54769d461c8ef1f1e8895f34a57e1f6
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-04-11 15:56:47 +0200

    (automated command)

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,18 +6,19 @@ import requests, json
 def top_ten(subreddit):
     """a function that queries the Reddit API and prints the titles
     of the first 10 hot posts listed for a given subreddit."""
-
+    number_of_titles = 0
     r = requests.get(
         "https://www.reddit.com/r/{}/hot.json".format(subreddit),
         headers={"User-Agent": "Custom"},
-        params={"limit": 10},
     )
     Data: dict = r.json()
     if r.status_code == 200:
         for i in range(len(Data["data"]["children"])):
             print(Data["data"]["children"][i]["data"]["title"])
+            number_of_titles += 1
     else:
         print(None)
+    print(number_of_titles)
 
 
 if "__main__" == __name__:
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,43 @@
 #!/usr/bin/python3
+import json
+import requests
+import sys
+
+
+def recurse(subreddit, hot_list: list = [], after=None):
+    """a function that queries the Reddit API and return a list
+    of all  hot posts titles  for a given subreddit."""
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    params = {"limit": 100, "after": after} if after else {"limit": 100}
+
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code != 200:
+        return None
+
+    data = response.json()
+
+    for child in data["data"]["children"]:
+        hot_list.append(child["data"]["title"])
+
+    if data["data"].get("after") is not None:
+        return recurse(subreddit, hot_list, data["data"]["after"])
+
+    return hot_list
+
+
+# Data["data"]["children"][i]["data"]["title"]
+
+
+if __name__ == "__main__":
+    recurse = __import__("2-recurse").recurse
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        result = recurse(sys.argv[1])
+        if result is not None:
+            print(len(result))
+        else:
+            print("None")
